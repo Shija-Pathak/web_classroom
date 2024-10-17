fc20bc6ddc6ffa5062b06030e69f9f7a1ad2abd9
Shija Pathak <shija2772@gmail.com>
2024-10-17 16:13:25 +0545

    remote instructions added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // git init -- to initialize the project (dont do this when working on a project because this breaks the project)
 // git status -- to check the project status
 // git help or git-h
-// git add index.js -- to add a file to the staging area, if we dont do this is stays in the working directory
+// git add file name -- to add a file to the staging area, if we dont do this is stays in the working directory
+// example: git add index.js
 // git add . --add all files
-
+// git remote -v -- view the remote origin (push and fetch link)
+// git push -u origin main -- to push
 
 //configuring git
 
