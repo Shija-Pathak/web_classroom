d59b47446fe1b4830017f854956b0d7ff322aded
Shija Pathak <shija2772@gmail.com>
2024-10-17 16:33:04 +0545

    f1 branch added

--- index.js
+++ index.js
@@ -5,15 +5,15 @@
 // example: git add index.js
 // git add . --add all files
 // git remote -v -- view the remote origin (push and fetch link)
-// git push -u origin main -- to push
+// git push -u origin main -- to push ( )
 //configuring git
-// git commit -m "files added"
+// git commit -m "files added"(in the "" write message mostly of what you changed or added)
 // git remote add origin link
 // git log -- shows commit history
 // git log --oneline  (gives your commit history in one line)
-// checkout -b feature1
-
-
+// checkout -b feature1 -- to create a new branch
+// git push -u origin feature1 ( feature 1 is the branch name) -- creates a new branch
+// git checkout main ( takes you to the main branch, main is the branch name) -- to switch branch
 
 
 //steps: add, commit, push
