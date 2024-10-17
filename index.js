b01b3a36f3625cde2bab991fece0f0602bd05a4b
Shija Pathak <shija2772@gmail.com>
2024-10-17 15:27:24 +0545

    files added

---
+++ index.js
@@ -0,0 +1,9 @@
+// git init -- to initialize the project (dont do this when working on a project because this breaks the project)
+// git status -- to check the project status
+// git help or git-h
+// git add index.js -- to add a file to the staging area, if we dont do this is stays in the working directory
+// git add . --add all files
+
+
+//configuring git
+
